310c84fbec5744d3c1cf0eed8e1ef1e26c499928
VATSU_04 <vatsumalambo@gmail.com>
2026-03-26 17:18:22 +0200

    feat: cree les tables

--- database/migrations/2026_03_26_150619_create_t_etudians_table.php
+++ database/migrations/2026_03_26_150619_create_t_etudians_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('t_etudians', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('age');
+            $table->integer('phone');
             $table->timestamps();
         });
     }
