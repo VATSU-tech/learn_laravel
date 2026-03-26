4dda90edbf39c411ce3abfacf3963fc509cd2b84
VATSU_04 <vatsumalambo@gmail.com>
2026-03-26 10:05:19 +0200

    feat: faire de la page liste la page d'acceuill par defaut

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/liste', function () {
     return view('welcome');
 });
 
@@ -14,6 +14,6 @@ Route::get('/login', function () {
     return view('login');
 });
 
-Route::get('/liste',function(){
+Route::get('/',function(){
     return view('liste');
 });
