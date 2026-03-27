6286dca989e6582454bc6f2314aeb0b2a1f6e42b
VATSU_04 <vatsumalambo@gmail.com>
2026-03-27 10:19:12 +0200

    delete

--- app/Http/Controllers/controllerEtudiant.php
+++ app/Http/Controllers/controllerEtudiant.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\etudiant as ModelsEtudiant;
 use Illuminate\Http\Request;
-use App\Modesl\etudiant;
 
 class controllerEtudiant extends Controller
 {
