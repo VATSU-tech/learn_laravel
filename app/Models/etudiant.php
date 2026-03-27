1a979992c4130dfbcdf50ac15c69209e1ba8efdd
VATSU_04 <vatsumalambo@gmail.com>
2026-03-27 10:09:16 +0200

    feat: corriger le nom de la table

--- app/Models/etudiant.php
+++ app/Models/etudiant.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class etudiant extends Model
 {
     //
-    protected $table = "t_etudiants";
+    protected $table = "t_etudians";
     protected $fillable = ["name","age","phone"];
 }
