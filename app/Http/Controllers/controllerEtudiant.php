ffa99ba021981fea8c428c5b9b249dc8863dd1d8
VATSU_04 <vatsumalambo@gmail.com>
2026-03-27 09:32:41 +0200

    feat:  afficher le model etudiant et recuperer les donnees

--- app/Http/Controllers/controllerEtudiant.php
+++ app/Http/Controllers/controllerEtudiant.php
@@ -11,6 +11,19 @@ class controllerEtudiant extends Controller
     //
     public function index()
     {
+    
+        //
         $etudiant = ModelsEtudiant::all();
+
+        return view('etudiants', ['etudiants'=> $etudiant]);
+    }
+
+    public function store(Request $request)
+    {   ModelsEtudiant::create([
+        'name' => $request->name,
+        'age' => $request->age,
+        'phone' => $request->phone
+    ]);
+        return redirect('/');
     }
 }
